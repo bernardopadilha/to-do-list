de991e9071e67cdc1e1c96370c553fc6cea0c10c
Bernardo <bernardo_limas@hotmail.com>
2024-03-18 13:40:04 -0300

    fix: arrumei as importacoes de imagens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import '../../public/rocket.png';
+import rocket from "../assets/rocket.png"
 
 export function Header() {
   return (
     <div className="w-full h-[200px] bg-[#0D0D0D] flex justify-center items-center">
-      {/* <img src="rocket.png" /> */}
+      <img src={rocket} />
       <h1 className="text-4xl ml-3 font-bold text-[#4EA8DE]">
         to<span className="text-[#8284FA]">do</span>
       </h1>
